eb384fbae7f571df85507054eb46efcce9a7966c
NEBULA-wr <alexyoulove754@gmail.com>
2025-06-25 18:02:53 -0600

    Corrijo nombres de archivos para despliegue

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi Aplicación del Clima</h1>
+        <p>¡La aplicación se está cargando!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
